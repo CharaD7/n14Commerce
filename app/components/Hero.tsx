de6535641d84f4bd71b335918f0adf6e47246455
CharaD7 <jijakahn6@gmail.com>
2023-11-17 04:54:05 +0000

    Added section to Hero page

---
+++ app/components/Hero.tsx
@@ -0,0 +1,12 @@
+
+export default function Hero() {
+  return (
+    <section className="mx-auto max-w-2xl px-4 sm:pb-6 lg:max-w-7xl lg:pt-8">
+      <div className="mb-8 flex flex-wrap justify-between mb:mb-16">
+        <div className="mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-1/3 lg:pb-24 lg:pt-48">
+          <h1 className="mb-4 text-4xl font-bold text-black sm:text-5xl md:mb-8md:text-6xl">Top Fashion for a top price</h1>
+        </div>
+      </div>
+    </section>
+  )
+}
