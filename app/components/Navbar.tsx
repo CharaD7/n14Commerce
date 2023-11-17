f9b60d3790b3a31ea99b82ee15ac822d2ab29423
CharaD7 <jijakahn6@gmail.com>
2023-11-17 04:35:08 +0000

    Reduced sitename header size for small screens

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -20,7 +20,7 @@ function Navbar() {
     <header className="mb-8 border-b">
       <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
         <Link href="/">
-          <h1 className="text-4xl font-bold">N14<span className="text-primary">Commerce</span></h1>
+          <h1 className="text-2xl md:text-4xl font-bold">N14<span className="text-primary">Commerce</span></h1>
         </Link>
 
         <nav className="hidden gap-12 lg:flex 2xl:ml-16">
