e8bfd9a1989a7fc6dfa32d12dcfbea2a40d7128c
CharaD7 <jijakahn6@gmail.com>
2023-11-17 04:32:34 +0000

    Added Shopping cart button

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,6 +2,8 @@
 
 import Link from "next/link"
 import { usePathname } from "next/navigation";
+import { Button } from "~/components/ui/button";
+import { ShoppingBag } from 'lucide-react';
 
 // Create links to pages
 const links = [
@@ -16,7 +18,7 @@ function Navbar() {
 
   return (
     <header className="mb-8 border-b">
-      <div className="flex items-center mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
+      <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
         <Link href="/">
           <h1 className="text-4xl font-bold">N14<span className="text-primary">Commerce</span></h1>
         </Link>
@@ -32,6 +34,13 @@ function Navbar() {
             </div>
           ))}
         </nav>
+        {/* Button call */}
+        <div className="flex divide-x border-r sm:border-l">
+          <Button variant={"outline"} className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none">
+            <ShoppingBag />
+            <span className="hidden text-xs font-semibold text-gray-500 sm:block">Cart</span>
+          </Button>
+        </div>
       </div>
     </header>
   )
