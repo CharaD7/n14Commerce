667d90f566a4aaaa26e6cea67caed87a00acf65a
CharaD7 <jijakahn6@gmail.com>
2023-11-26 18:59:10 +0000

    Render Newest component in page.tsx

--- app/components/Newest.tsx
+++ app/components/Newest.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { client } from '../lib/sanity';
 import { simplifiedProduct } from '../interface';
+import Link from 'next/link';
 
 async function getData() {
   const query = `
@@ -20,4 +21,15 @@ async function getData() {
 
 export default async function Newest() {
   const data: simplifiedProduct = await getData();
+
+  return (
+    <div className='bg-white'>
+      <div className='mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8'>
+        <div className='flex flex-between items-center'>
+          <h2 className='text-2xl font-bold tracking-tight text-gray-900'>Our Newest Products</h2>
+          <Link href="/all">See all</Link>
+        </div>
+      </div>
+    </div>
+  )
 }
