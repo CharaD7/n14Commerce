b5aad9d92fdc36cbd969993aa74580ad1e5c41dd
CharaD7 <jijakahn6@gmail.com>
2023-11-27 09:27:02 +0000

    Added product name-category-price to listing

--- app/components/Newest.tsx
+++ app/components/Newest.tsx
@@ -37,6 +37,8 @@ export default async function Newest() {
         <div className='mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
           {data.map((product) => (
             <div key={product._id} className='group relative'>
+
+              {/* Image div */}
               <div className='aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80'>
                 <Image
                   src={product.imageUrl}
@@ -45,6 +47,17 @@ export default async function Newest() {
                   width={300}
                   height={300}
                 />
+              </div>
+
+              {/* Product details div */}
+              <div className='mt-4 flex justify-between'>
+                <div>
+                  <h3 className='text-sm text-gray-700'>
+                    <Link href={`/product/${product.slug}`}>{product.name}</Link>
+                  </h3>
+                  <p className='mt-1 text-sm text-gray-500'>{product.categoryName}</p>
+                </div>
+                <p className='text-sm font-medium text-gray-900'>${product.price}</p>
               </div>
             </div>
           ))}
