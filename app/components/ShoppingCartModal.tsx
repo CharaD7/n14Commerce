408231b30e7daf586f0340db0ceaf40deeb2eac9
CharaD7 <jijakahn6@gmail.com>
2023-12-12 23:41:33 +0000

    Added Shopping cart modal hooks to ShoppingCartModal component

--- app/components/ShoppingCartModal.tsx
+++ app/components/ShoppingCartModal.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useShoppingCart } from "use-shopping-cart";
 import {
   Sheet,
   SheetContent,
@@ -6,15 +9,25 @@ import {
 } from "~/components/ui/sheet"
 
 export default function ShoppingCartModal() {
+  const { cartCount, shouldDisplayCart, handleCartClick } = useShoppingCart();
+
   return (
-    <Sheet defaultOpen>
-      <SheetContent className="sm:max-w-lg w-[90vw]">
+    <Sheet open={shouldDisplayCart} onOpenChange={() => handleCartClick()}>
+      <SheetContent className="sm:max-w-lg w-[90vw] font-sans">
         <SheetHeader>
-          <SheetTitle>Your Checkout</SheetTitle>
+          <SheetTitle>Shopping Cart Items</SheetTitle>
         </SheetHeader>
 
-        <div>
-          <h1>Well, hello there!</h1>
+        <div className="h-full flex flex-col justify-between">
+          <div className="mt-8 flex-1 overflow-y-auto">
+            <ul className="-my-6 divide-y divide-gray-200">
+              { cartCount === 0 ? (
+                <h1 className="py-6">You do not have any items in your cart.</h1>
+              ) : (
+                <h1>Hey, you have some items</h1>
+              ) }
+            </ul>
+          </div>
         </div>
       </SheetContent>
     </Sheet>
