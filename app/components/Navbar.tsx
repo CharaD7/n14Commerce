1b1ff2e791e2e67747535b69103e56e0f91715fd
CharaD7 <jijakahn6@gmail.com>
2023-12-12 23:43:28 +0000

    Added click event to Cart button to show SheetModal

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link"
 import { usePathname } from "next/navigation";
 import { Button } from "~/components/ui/button";
 import { ShoppingBag } from 'lucide-react';
+import { useShoppingCart } from "use-shopping-cart";
 
 // Create links to pages
 const links = [
@@ -15,6 +16,7 @@ const links = [
 
 function Navbar() {
   const pathname = usePathname();
+  const { handleCartClick } = useShoppingCart();
 
   return (
     <header className="mb-8 font-sans border-b">
@@ -36,7 +38,7 @@ function Navbar() {
         </nav>
         {/* Button call */}
         <div className="flex divide-x border-r sm:border-l">
-          <Button variant={"outline"} className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none">
+          <Button variant={"outline"} className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none" onClick={() => handleCartClick() }>
             <ShoppingBag />
             <span className="hidden text-xs font-semibold text-gray-500 sm:block">Cart</span>
           </Button>
