08eb0c4dceeaa9c10122ff07760e4c80f921f3b3
CharaD7 <jijakahn6@gmail.com>
2024-01-28 20:17:07 +0000

    Designed success page

---
+++ app/stripe/success/page.tsx
@@ -0,0 +1,26 @@
+import { CheckCheck } from 'lucide-react';
+import Link from 'next/link';
+import React from 'react'
+import { Button } from '~/components/ui/button';
+
+const SuccessStripe = () => {
+  return (
+    <div className='h-screen'>
+      <div className='mt-32 md:max-w-[50vw] mx-auto'>
+        <CheckCheck className='text-green-500 w-16 h-16 mx-auto my-6' />
+        <div className='text-center'>
+          <h3 className='md:text-2xl text-base text-gray-900 font-semibold text-center'>Payment Successful!</h3>
+          <p className='text-gray-600 my-2'>Thank you for your purchase, we hope you enjoy using our products.</p>
+          <p>Have a great day</p>
+
+          <Button asChild className='mt-5'>
+            <Link href='/'>Go Back</Link>
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SuccessStripe;
+
